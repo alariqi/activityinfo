1c5315981e4591cfe16dd03fd68939ba2e051e71
Alex Bertram <alex@bedatadriven.com>
2018-02-22 19:28:13 +0100

    Added GZIP encoding for query results
    Compressing the results on the fly isn't possible within the
    AppEngine environment (responses are cached and compressed
    on the edge servers), but for deployments to Azure it's very useful.

--- store/apiserver/src/main/java/org/activityinfo/store/server/QueryResource.java
+++ store/apiserver/src/main/java/org/activityinfo/store/server/QueryResource.java
@@ -10,11 +10,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
 
 
 public class QueryResource {
+
+    private static final boolean APPENGINE = (System.getenv("com.google.appengine.runtime.environment") != null);
 
     private final ApiBackend backend;
 
@@ -26,22 +27,55 @@ public class QueryResource {
     @Path("columns")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response queryColumns(QueryModel model) {
+    public Response queryColumns(@HeaderParam("Accept-Encoding") String acceptEncoding, QueryModel model) {
 
         ColumnSetBuilder builder = backend.newQueryBuilder();
 
         final ColumnSet columnSet = builder.build(model);
 
-        final StreamingOutput output = new StreamingOutput() {
-            @Override
-            public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-                ColumnJsonWriter columnSetWriter = new ColumnJsonWriter(outputStream, Charsets.UTF_8);
-                columnSetWriter.write(columnSet);
-                columnSetWriter.flush();
-            }
+        // If the client supports it, write the JSON directly to
+        // a Gzip output stream.
+
+        // In the AppEngine environment, all content-encoding is handled
+        // by the surrounding runtime, so this is not option here.
+
+        if(!APPENGINE && clientSupportsGzip(acceptEncoding)) {
+            return streamingCompressedResponse(columnSet);
+        } else {
+            return streamingResponse(columnSet);
+        }
+    }
+
+    private boolean clientSupportsGzip(String acceptEncoding) {
+        if(acceptEncoding == null) {
+            return false;
+        }
+        return acceptEncoding.toLowerCase().contains("gzip");
+    }
+
+    private Response streamingResponse(ColumnSet columnSet) {
+
+        StreamingOutput output = outputStream -> {
+            ColumnJsonWriter columnSetWriter = new ColumnJsonWriter(outputStream, Charsets.UTF_8);
+            columnSetWriter.write(columnSet);
+            columnSetWriter.flush();
+        };
+        return Response.ok(output).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+    private Response streamingCompressedResponse(ColumnSet columnSet) {
+        StreamingOutput output = outputStream -> {
+            GZIPOutputStream zippedOut = new GZIPOutputStream(outputStream);
+            ColumnJsonWriter columnSetWriter = new ColumnJsonWriter(zippedOut, Charsets.UTF_8);
+            columnSetWriter.write(columnSet);
+            columnSetWriter.flush();
+            zippedOut.finish();
         };
 
-        return Response.ok(output).type(MediaType.APPLICATION_JSON_TYPE).build();
+        return Response.ok(output)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .header("Content-Encoding", "gzip")
+                .build();
     }
 
 }
