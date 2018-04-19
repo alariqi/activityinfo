f004695d04fe80086e760ced5e1d334340b0ef43
Alex Bertram <alex@bedatadriven.com>
2018-04-19 11:44:04 +0200

    Added "button--active" class to menu button when dropdown is active

--- ui/app/src/main/java/org/activityinfo/theme/client/base/button/MenuButton.java
+++ ui/app/src/main/java/org/activityinfo/theme/client/base/button/MenuButton.java
@@ -1,6 +1,5 @@
 package org.activityinfo.theme.client.base.button;
 
-import com.sencha.gxt.cell.core.client.ButtonCell;
 import com.sencha.gxt.widget.core.client.button.CellButtonBase;
 import com.sencha.gxt.widget.core.client.menu.Menu;
 
@@ -8,7 +7,7 @@ import com.sencha.gxt.widget.core.client.menu.Menu;
 public class MenuButton extends CellButtonBase<String> {
 
     public MenuButton(String label, Menu menu) {
-        super(new ButtonCell<>(new MenuButtonAppearance()), label);
+        super(new MenuButtonCell(), label);
         setMenu(menu);
     }
 }
